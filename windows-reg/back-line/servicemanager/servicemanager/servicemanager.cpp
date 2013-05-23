38b98522addd2cb8c974eabea5b0026e78e64ad4
zengfanbo2009 <zengfanbo2009@gmail.com>
2013-05-23 13:22:34 +0800

    add enum function

--- windows-reg/back-line/servicemanager/servicemanager/servicemanager.cpp
+++ windows-reg/back-line/servicemanager/servicemanager/servicemanager.cpp
@@ -36,9 +36,37 @@ int _tmain(int argc, _TCHAR* argv[])
 		return -1 ;
 	}
 
+	// enum the service 
+	// 1. prepare the param we need in the EnumServiceStatus
+	// 2. call the function 
+	// 3. display the result of the function we get
+	// 4. release the dynamic memory
+
+	LPENUM_SERVICE_STATUS service_status_Result ;
+	DWORD dword_BytesNeed = 0;
+	DWORD dword_SerivceReturned = 0;
+	DWORD dword_ResumeHandle = 0;
+
+	service_status_Result = (LPENUM_SERVICE_STATUS)LocalAlloc(LPTR,64 * 1024) ;
+	EnumServicesStatus(handle_opened_Manager,SERVICE_WIN32,SERVICE_STATE_ALL,service_status_Result,
+						64 * 1024,&dword_BytesNeed,&dword_SerivceReturned,&dword_ResumeHandle) ;
+
+	for (DWORD  i = 0 ; i < dword_SerivceReturned; ++i)
+	{
+		wprintf(L"服务名称:%s\t\t",service_status_Result[i].lpServiceName) ;
+		wprintf(L"显示名称:%s\r\n",service_status_Result[i].lpDisplayName) ;
+	}
+	if (dword_SerivceReturned == 0)
+	{
+		GetLastError();
+	}
+
+	LocalFree(service_status_Result) ;
 	CloseServiceHandle(handle_opened_service) ;
 	CloseServiceHandle(handle_opened_Manager) ;
 	
+	getwchar() ;
+
 	return 0;
 }
 
